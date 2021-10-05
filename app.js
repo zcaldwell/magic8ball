e59d90f63a67cc96d37f0d14eb4aa6e8fcbd37e2
zcaldwell <mintyst.john@gmail.com>
2021-10-05 15:50:50 -0700

    animation working but need to reset it somehow

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const getRandomNumber = (max)=>{
   return Math.floor(Math.random() * max);
 };
 
-
-
-const buttonPress = document.getElementById('shake-8ball')
-const answer = document.getElementById('answer');
+const buttonPress = document.getElementById('shake-8ball');
+const answer = document.getElementById('8ballanswer');
+const shakeBall = document.getElementById('theball');
 
 buttonPress.addEventListener('click', ()=> {
   const randomAnswer = getRandomNumber(answers.length);
+  shakeBall.classList.add('shake');
+  document.getElementById('8ballanswer').className = 'hide';
+  document.getElementById('8ballanswer').className = 'show';
+  onclick="reset_animation()"
   return answer.textContent = answers[randomAnswer];
-  
+
 })
