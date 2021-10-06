a4dec6eaf7258e1fdd525134cf5ed081da89b180
zcaldwell <mintyst.john@gmail.com>
2021-10-05 17:11:48 -0700

    working on CSS, animation reset finishd

--- app.js
+++ app.js
@@ -7,13 +7,16 @@ const getRandomNumber = (max)=>{
 const buttonPress = document.getElementById('shake-8ball');
 const answer = document.getElementById('8ballanswer');
 const shakeBall = document.getElementById('theball');
+const playAudio = document.getElementById('audio')
 
 buttonPress.addEventListener('click', ()=> {
   const randomAnswer = getRandomNumber(answers.length);
+  playAudio.play();
+  shakeBall.classList.remove('shake');
+  void shakeBall.offsetWidth;
   shakeBall.classList.add('shake');
   document.getElementById('8ballanswer').className = 'hide';
   document.getElementById('8ballanswer').className = 'show';
-  onclick="reset_animation()"
   return answer.textContent = answers[randomAnswer];
 
 })
