9906f215bce5ae0edb6f74d49f1453d3ee089758
zcaldwell <mintyst.john@gmail.com>
2021-10-05 17:14:33 -0700

    fixing eslint stuff

--- app.js
+++ app.js
@@ -1,22 +1,21 @@
 import { answers } from './answers.js';
 
 const getRandomNumber = (max)=>{
-  return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max);
 };
 
 const buttonPress = document.getElementById('shake-8ball');
 const answer = document.getElementById('8ballanswer');
 const shakeBall = document.getElementById('theball');
-const playAudio = document.getElementById('audio')
+const playAudio = document.getElementById('audio');
 
 buttonPress.addEventListener('click', ()=> {
-  const randomAnswer = getRandomNumber(answers.length);
-  playAudio.play();
-  shakeBall.classList.remove('shake');
-  void shakeBall.offsetWidth;
-  shakeBall.classList.add('shake');
-  document.getElementById('8ballanswer').className = 'hide';
-  document.getElementById('8ballanswer').className = 'show';
-  return answer.textContent = answers[randomAnswer];
-
-})
+    const randomAnswer = getRandomNumber(answers.length);
+    playAudio.play();
+    shakeBall.classList.remove('shake');
+    void shakeBall.offsetWidth;
+    shakeBall.classList.add('shake');
+    document.getElementById('8ballanswer').className = 'hide';
+    document.getElementById('8ballanswer').className = 'show';
+    return answer.textContent = answers[randomAnswer];
+});
